4350535270c789bfa218ad6c7ca4560d0d1f7a01
nagel <kevin.jerome.nagel@gmail.com>
2018-10-11 20:58:31 -0500

    Loop through data instance attempt 1

--- app/controllers/api/cables_controller.rb
+++ app/controllers/api/cables_controller.rb
@@ -1,7 +1,11 @@
 class Api::CablesController < ApplicationController
 
-  def test_method
+  def display_all
+
+    #@cables is an instance of the Cable class and contains all the data instances (in an array) from the Cable model
+    @cables = Cable.all
     render "view.json.jbuilder"
+
   end 
 
 end
--- app/views/api/cables/view.json.jbuilder
+++ app/views/api/cables/view.json.jbuilder
@@ -1,1 +1,14 @@
-json.test "hello"
+
+
+@cables.each do |cable| 
+  json.name cable.name
+  json.price cable.price
+  json.length cable.length
+  json.function cable.function
+  json.model_year cable.model_year
+  json.cable cable.description
+end
+
+
+
+
