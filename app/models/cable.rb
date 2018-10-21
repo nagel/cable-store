87ed21da88ddb33d95be45e3a6b67904d4e9cbce
nagel <kevin.jerome.nagel@gmail.com>
2018-10-21 10:12:02 -0500

    Added basic validation when creating cable data model instance.

--- app/models/cable.rb
+++ app/models/cable.rb
@@ -1,5 +1,12 @@
 class Cable < ApplicationRecord
 
+  validates :price, numericality: {greater_than: 0}
+  validates :price, presence: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :description, length: {in: 50..100}
+
+
   def is_discounted?
     if self.price < 10
       "Discounted!"
